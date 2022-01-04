c2460fe74f22e50f160e88c55158c0a8570419ac
Mahendra Vishwakarma <mahendra010895@gmail.com>
2022-01-04 18:22:05 +0530

    code improved

--- ChatBot/Scenes/ViewModel/ChatViewModel.swift
+++ ChatBot/Scenes/ViewModel/ChatViewModel.swift
@@ -16,7 +16,12 @@ class ChatViewModel {
         totalChatMessage = Array<Message>()
         let msgTimeStamp = Date().timeIntervalSince1970
         let msg  = Message(chatBotName: "", chatBotID: 5, message: "Message from remote user", emotion: "", timestamp: msgTimeStamp)
+        let msg1  = Message(chatBotName: "", chatBotID: 5, message: "Hi", emotion: "", timestamp: msgTimeStamp)
+        let msg2  = Message(chatBotName: "", chatBotID: 5, message: "How are you?", emotion: "", timestamp: msgTimeStamp)
         totalChatMessage?.append(msg)
+        totalChatMessage?.append(msg1)
+        totalChatMessage?.append(msg2)
+        
     }
     
     func sendMessage(messgae:String)  {
@@ -24,22 +29,22 @@ class ChatViewModel {
         let msg  = Message(chatBotName: "", chatBotID: 6, message: messgae, emotion: "", timestamp: msgTimeStamp)
         totalChatMessage?.append(msg)
         self.delegate?.update(status: .success)
-        return
         
-        let requestURL = Utils.cityURL
-        Webservices.requesData(url: requestURL) { [weak self] (result:Result<ChatModel,APIError>) in
-            switch result {
-            case .success(let res):
-                if res.success == 0 {
-                    self?.delegate?.update(status: .success)
-                }else {
-                    self?.delegate?.update(status: .failed)
-                }
-               
-            case .failure(_):
-                self?.delegate?.update(status: .failed)
-            }
-        }
+        
+//        let requestURL = Utils.cityURL
+//        Webservices.requesData(url: requestURL) { [weak self] (result:Result<ChatModel,APIError>) in
+//            switch result {
+//            case .success(let res):
+//                if res.success == 0 {
+//                    self?.delegate?.update(status: .success)
+//                }else {
+//                    self?.delegate?.update(status: .failed)
+//                }
+//
+//            case .failure(_):
+//                self?.delegate?.update(status: .failed)
+//            }
+//        }
     }
 }
 
